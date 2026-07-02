a8ad820724a552c4c5bebe9f45257971f5ea39b3
supriyankabudhathoki. <budhathokisupri45@gmail.com.>
2026-07-02 07:00:21 +0545

    classromm update

---
+++ A_basics/user_defined_num/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Mail;
+
+namespace user_defined_num;
+
+class Program
+
+{
+static void Main(string[] args)
+{
+int a = 10; 
+int b= 20;
+        
+Console.WriteLine("sum  is" + ( a+b));
+    }
+}
