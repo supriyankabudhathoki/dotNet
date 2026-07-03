820f2b35945dce740cd62cff4ad787f824882090
supriyankabudhathoki. <budhathokisupri45@gmail.com.>
2026-07-03 08:33:00 +0545

    adding datatype conversion

---
+++ A_basics/type_conversion/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace type_conversion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+/*
+        byte a1= 255;//due to implicit conversion  widening it cannot take 256 ok
+        byte a= 10;
+       / int  p= a; 
+
+        double b = 255.55;
+        int c =(int)a;
+
+        /* float b =a; 
+        float b = (float)a;*/
+
+        string a = "123";
+        
+       /*  int b= Convert.ToInt32(a); */
+       //demonstrating datatype conversion using parse
+        int b = int.Parse(a);
+        int c = 345;
+        int sum = b+c;
+
+    Console.WriteLine("sum ="+ sum);
+
+        /* Console.WriteLine("double value="+ a);
+        Console.WriteLine("Byte value="+ b);
+        Console.WriteLine("converted integer value="+ c); */
+
+    }
+}
