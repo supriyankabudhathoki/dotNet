a6922e06a504a10e5caa1cbcb0e8fc13686f186c
supriyankabudhathoki. <budhathokisupri45@gmail.com.>
2026-07-16 08:38:09 +0545

    Interface is added on heritance

---
+++ f_inheritance/d_multiple_inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace a_interface;
+
+interface  IDad
+{
+    public void hi()
+    {
+        Console.WriteLine("Hello from your dad.");
+    }
+}
+interface  IMom
+{
+    public void Hello()
+    {
+        Console.WriteLine("Hello from your mother.");
+    }
+}
+
+class Child : IDad, IMom {
+    public void hi()
+    {
+        Console.WriteLine("Hi dad and mom");
+    }
+
+    public void Hello()
+    {
+        Console.WriteLine("Hello dad and mom");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Child c1 = new Child();
+        c1.hi();
+        c1.Hello();
+    }
+}
---
+++ f_inheritance/g_intefacewithinterface/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace g_intefacewithinterface;
+public interface IBasicPrinter
+{
+    void Print(); 
+}
+
+public interface ISmartPrinter : IBasicPrinter
+{
+    void Scan(); 
+}
+
+public class OfficePrinter : ISmartPrinter
+{
+    public void Print()
+    {
+        System.Console.WriteLine("Printing paper...");
+    }
+    public void Scan()
+    {
+        System.Console.WriteLine("Scanning document...");
+    }
+}
