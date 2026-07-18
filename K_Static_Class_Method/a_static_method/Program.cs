c9321545fad3986b45e8d077a9eb99d77bfd59eb
supriyankabudhathoki. <budhathokisupri45@gmail.com.>
2026-07-18 22:33:31 +0545

    static method done

---
+++ K_Static_Class_Method/a_static_method/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace a_static;
+
+static class Shape
+{
+    public static int length = 23;
+    public static int squareArea()
+    {
+        return (length * length);
+    }
+    public static int cube(int a)
+    {
+        return (a * a * a);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("The length of the shape = " + Shape.length);
+        Console.WriteLine("The area of the square = " + Shape.squareArea() + " u. sq.");
+        Console.WriteLine("The cube of the given number = " + Shape.cube(5));
+    }
+}
---
+++ K_Static_Class_Method/c_static_method/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace c_static;
+
+class Employee
+{
+    public static string companyName = "Microsoft";
+    public int empId;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee e1 = new Employee();
+        e1.empId = 56;
+
+        Console.WriteLine("Employee id = " + e1.empId);
+        Console.WriteLine("Company name = " + Employee.companyName);
+
+        Employee e2 = new Employee();
+        e2.empId = 23;
+        Console.WriteLine("Employee id = " + e2.empId);
+        Console.WriteLine("Company name = " + Employee.companyName);
+
+        Employee e3 = new Employee();
+        e3.empId = 33;
+        Employee.companyName = "Google";
+        Console.WriteLine("Employee id = " + e3.empId);
+        Console.WriteLine("Company name = " + Employee.companyName);
+    }
+}
