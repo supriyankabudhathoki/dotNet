8ab5bb15e98e5f322b919390cd7301a0ffb3f8f2
supriyankabudhathoki. <budhathokisupri45@gmail.com.>
2026-07-18 22:11:03 +0545

    enumeration in dotnet

---
+++ I_enum/b_enum/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace a_enum;
+
+class Program
+{
+    enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+    static void Main(string[] args)
+    {
+        int lastDay = (int)Days.Saturday;
+        Console.WriteLine(lastDay);
+
+        var wd = (Days)5;
+        Console.WriteLine(wd);
+    }
+}
---
+++ I_enum/c_enum/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace c_enum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name;
+        Days day;
+        Console.WriteLine("Enter name:");
+        name = Console.ReadLine();
+        Console.WriteLine("Enter values from (0-6) for day:");
+        int index = int.Parse(Console.ReadLine());
+        day = (Days)index;
+        Console.WriteLine("Hello " + name + " today is " + day + ".");
+    }
+
+    enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+}
