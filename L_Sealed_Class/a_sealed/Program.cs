3db0a8825df6faf09de41417f1ef892e8c3bbee4
supriyankabudhathoki. <budhathokisupri45@gmail.com.>
2026-07-18 22:36:18 +0545

    sealed class

---
+++ L_Sealed_Class/a_sealed/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace a_sealed;
+
+// sealed class SuperClass
+class SuperClass
+{
+    public void hi()
+    {
+        Console.WriteLine("Hi, friends!.");
+    }
+}
+class SubClass : SuperClass
+{
+    public void hello()
+    {
+        Console.WriteLine("Hello Sanothimi");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SubClass obj = new SubClass();
+        obj.hi();
+        obj.hello();
+    }
+}
