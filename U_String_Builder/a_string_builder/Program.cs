8e103c0ed7bdbd3140f1745f6fc213290cf97f15
supriyankabudhathoki. <budhathokisupri45@gmail.com.>
2026-08-06 06:33:11 +0545

    string builder

---
+++ U_String_Builder/a_string_builder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace a_string_builder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Hello";
+        StringBuilder a = new StringBuilder(str);
+        Console.WriteLine("s");
+    }
+}
---
+++ U_String_Builder/b_satring_builder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace a_string_builder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        StringBuilder s = new StringBuilder("Hello sanothimi");
+           string str = s.ToString();
+        Console.WriteLine("str");
+    }
+}
